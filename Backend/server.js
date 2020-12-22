5c01591ad5c0687305aba894c939b6bd465d68d4
edvaldo gjonikaj <edvaldo99@icloud.com>
2020-12-22 14:20:41 +0100

    ok

--- Backend/server.js
+++ Backend/server.js
@@ -4,16 +4,12 @@ const helmet = require('helmet');
 const users = require('./api/users');
 const quotes = require('./api/quotes')
 const app = express();
-const cors = require('cors')
-var corsOptions = {
-  origin: 'http://localhost:3000',
-  optionsSuccessStatus: 200 
-}
+const path = require('path');
+
+const PORT = process.env.PORT || 5000
+
 app.use(helmet());
-
 app.use(express.json())
-
-app.use(cors(corsOptions))
 
 mongoose.connect('mongodb://localhost:27017/todolist', {useNewUrlParser: true,useFindAndModify:false,useCreateIndex:true,useUnifiedTopology: true});
 
@@ -24,4 +20,8 @@ app.get('/', function (req, res) {
   app.use('/users', users);
   app.use('/quotes', quotes);
 
-app.listen(5000, () => console.log(`Server started on port 5000`))
+  if(process.env.NODE_ENV=="production"){
+    app.use(express.static('frontend/build'))
+  }
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
